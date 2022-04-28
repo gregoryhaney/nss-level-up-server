7f9cb07776870303e00c5f09188bebaddc2ab216
Gregory Haney <gregory.haney@gmail.com>
2022-04-28 15:02:32 -0500

    MAJOR: Up through Chapter 7

--- scratchpad.sql
+++ scratchpad.sql
@@ -4,3 +4,9 @@ SELECT * FROM auth_user;
 SELECT * FROM authtoken_token;
 SELECT * FROM levelupapi_gamer;
 SELECT * FROM levelupapi_event;
+
+DELETE FROM levelupapi_game WHERE id=2
+
+INSERT INTO levelupapi_game VALUES(2, "Pac Man", "ColecoVision", 2, 4, 3, 1);
+INSERT INTO levelupapi_game VALUES(3, "Asteroids", "Konami", 1, 1, 2, 1);
+
